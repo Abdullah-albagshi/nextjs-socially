bf43545222ea345ce0fd582c9743acde9a21c070
Abdullah albagshi <abdullah.albagshi@gmail.com>
2025-03-12 00:31:01 +0300

    Implement post interaction features in PostCard component
    - Added like, comment, and delete post functionality
    - Implemented optimistic UI updates for likes
    - Enhanced PostCard with user interaction methods
    - Added error handling and toast notifications for post actions

--- src/actions/posts.action.ts
+++ src/actions/posts.action.ts
@@ -7,7 +7,7 @@ import { revalidatePath } from 'next/cache';
 export async function createPost(content: string, imageUrl?: string) {
 	try {
 		const userId = await getDBUserId();
-    if(!userId) return;
+		if (!userId) return;
 		const post = await prisma.post.create({
 			data: {
 				content,
@@ -26,52 +26,181 @@ export async function createPost(content: string, imageUrl?: string) {
 	}
 }
 
-
 export async function getPosts() {
-  try {
-    const posts = await prisma.post.findMany({
-      orderBy:{
-        createdAt: 'desc'
-      },
-      include:{
-        author:{
-          select:{
-            name: true,
-            username:true,
-            image: true
-          }
-        },
-        comments:{
-          select:{
-            author: {
-              select:{
-                id:true,
-                username:true,
-                image:true,
-                name:true
-              }
-            }
-          },
-          orderBy:{
-            createdAt: 'asc'
-          }
-        },
-        likes:{
-          select:{
-            userId:true
-          }
-        },
-        _count:{
-          select:{
-            likes:true,
-            comments:true
-          }
-        }
-      }
-    })
+	try {
+		const posts = await prisma.post.findMany({
+			orderBy: {
+				createdAt: 'desc',
+			},
+			include: {
+				author: {
+					select: {
+						name: true,
+						username: true,
+						image: true,
+					},
+				},
+				comments: {
+					select: {
+						author: {
+							select: {
+								id: true,
+								username: true,
+								image: true,
+								name: true,
+							},
+						},
+					},
+					orderBy: {
+						createdAt: 'asc',
+					},
+				},
+				likes: {
+					select: {
+						userId: true,
+					},
+				},
+				_count: {
+					select: {
+						likes: true,
+						comments: true,
+					},
+				},
+			},
+		});
 
-    return posts;
-  } catch (error) {
-    console.error("Something went Wrong in getPosts",error)
+		return posts;
+	} catch (error) {
+		console.error('Something went Wrong in getPosts', error);
+	}
+}
+
+export async function toggleLike(postId: string) {
+	try {
+		const userId = await getDBUserId();
+		if (!userId) return;
+
+		// check if like exists
+		const existingLike = await prisma.like.findUnique({
+			where: {
+				userId_postId: {
+					userId,
+					postId,
+				},
+			},
+		});
+
+		const post = await prisma.post.findUnique({
+			where: { id: postId },
+			select: { authorId: true },
+		});
+
+		if (!post) throw new Error('Post Not Found');
+
+		if (existingLike) {
+			await prisma.like.delete({
+				where: {
+					userId_postId: {
+						userId,
+						postId,
+					},
+				},
+			});
+		} else {
+			prisma.$transaction([
+				prisma.like.create({
+					data: {
+						postId,
+						userId,
+					},
+				}),
+				...(post.authorId !== userId
+					? [
+							prisma.notification.create({
+								data: {
+									type: 'LIKE',
+									creatorId: postId,
+									postId: postId,
+									userId: post.authorId,
+								},
+							}),
+					  ]
+					: []),
+			]);
+			revalidatePath('/');
+			return { success: true };
+		}
+	} catch (error) {
+		console.log('Error Liking/Unlike Post', error);
+		return { success: false, error: 'Error Liking/Unlike Post' };
+	}
+}
+
+export async function createComment(postId: string, content: string) {
+	try {
+		const userId = await getDBUserId();
+		if (!userId) return;
+		if (!content) throw new Error('Content is Required');
+
+		const post = await prisma.post.findUnique({
+			where: { id: postId },
+			select: { authorId: true },
+		});
+
+		if (!post) throw new Error('Post Not Found');
+
+		const comment = await prisma.$transaction(async (tx) => {
+			const comment = await tx.comment.create({
+				data: {
+					content,
+					authorId: userId,
+					postId,
+				},
+			});
+
+			if (post.authorId !== userId) {
+				await tx.notification.create({
+					data: {
+						type: 'COMMENT',
+						userId: post.authorId,
+						creatorId: userId,
+						commentId: comment.id,
+						postId,
+					},
+				});
+			}
+
+			return comment;
+		});
+
+		revalidatePath('/');
+		return { success: true, content: comment };
+	} catch (error) {
+		console.log('Error Creating Comment', error);
+		return { success: false, error: 'Error Creating Comment' };
+	}
+}
+
+export async function deletePost(postId: string) {
+	try {
+		const userId = await getDBUserId();
+		if (!userId) return;
+		const post = await prisma.post.findUnique({
+			where: { id: postId },
+			select: { authorId: true },
+		});
+
+		if (!post) throw new Error('Post Not Found');
+    if (post.authorId !== userId) throw new Error("Unauthorized - Can't delete other Posts")
+		await prisma.post.delete({
+			where: { id: postId },
+		});
+
+		revalidatePath('/');
+		return { success: true };
+	} catch (error) {
+    console.log("Failed to delete Post", error);
+    return {success: false, error: "Failed to delete Post"};
   }
 }
+
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,24 +1,102 @@
-"use client";
+'use client';
 
-import { getPosts } from "@/actions/posts.action";
-import { Card } from "../ui/card";
+import {
+	createComment,
+	deletePost,
+	getPosts,
+	toggleLike,
+} from '@/actions/posts.action';
+import { Card, CardContent } from '../ui/card';
+import { useUser } from '@clerk/nextjs';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 
-
-type Posts = NonNullable<Awaited<ReturnType<typeof getPosts>>>
-type Post = Posts[number]
+type Post = NonNullable<Awaited<ReturnType<typeof getPosts>>>[number];
 
 interface PostCardProps {
-  post: Post
+	post: Post;
+	dbUserId: string | null;
 }
 
-const PostCard = ({ post }: PostCardProps) => {
-  return (
-    <Card>
-      <div>
-        {post.content}
-      </div>
-    </Card>
-  );
+const PostCard = ({ post, dbUserId }: PostCardProps) => {
+	const { user } = useUser();
+
+	const [newComment, setNewComment] = useState('');
+	const [isCommenting, setIsCommenting] = useState(false);
+	const [isLiking, setIsLiking] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [hasLiked, setHasLiked] = useState(
+		post.likes.some((like) => like.userId === dbUserId)
+	);
+	const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+
+	const handleLike = async () => {
+		if (isLiking) return;
+		try {
+			setIsLiking(true);
+			setHasLiked((prev) => !prev);
+			setOptimisticLikes((prev) => prev + (hasLiked ? -1 : 1));
+			await toggleLike(post.id);
+		} catch (error) {
+			setOptimisticLikes(post._count.likes);
+			setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+		} finally {
+			setIsLiking(false);
+		}
+	};
+
+	const handleAddComment = async () => {
+		if (!newComment.trim() || isCommenting) return;
+		try {
+			setIsCommenting(true);
+			const res = await createComment(post.id, newComment);
+			if (res?.success) {
+				toast.success('Comment Posted successfully!');
+				setNewComment('');
+			}
+		} catch (error) {
+			toast.error('Failed to add comment');
+		} finally {
+			setIsCommenting(false);
+		}
+	};
+
+	const handleDeletePost = async () => {
+		if (post.authorId !== user?.id || isDeleting) return;
+		try {
+			setIsDeleting(true);
+			const res = await deletePost(post.id);
+			if (res?.success) {
+				toast.success('Post Deleted Successfully!');
+			} else {
+				throw new Error(res?.error);
+			}
+		} catch (error) {
+			toast.error('Failed to deletePost');
+		} finally {
+			setIsDeleting(false);
+		}
+	};
+
+	return (
+		<Card className='overflow-hidden'>
+			<CardContent className='p-4 sm:p-6'>
+				<div className='py-4'>
+					<div className='flex gap-3 sm:gap-4'>
+						<Link href={`/profile/${post.author.username}`}>
+							<Avatar className='w-10 h-10 border-2'>
+								<AvatarImage src={post.author.image as string} />
+								<AvatarFallback>{post.author?.username}</AvatarFallback>
+							</Avatar>
+						</Link>
+					</div>
+				</div>
+			</CardContent>
+			<div>{post.content}</div>
+		</Card>
+	);
 };
 
-export default PostCard;  
+export default PostCard;
